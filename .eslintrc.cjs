e6f75e6e18e607a7d20c4534dca1859b76aa921d
luongdao <luong.dao@pionero.io>
2023-04-28 18:30:16 +0700

    init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    node: true,
+    jest: true,
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort', 'promise'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'plugin:promise/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsConfigRootDir: __dirname,
+    project: './tsconfig.json',
+    sourceType: 'module',
+    // Only ESLint 6.2.0 and later support ES2020.
+    ecmaVersion: 2020,
+  },
+  root: true,
+  rules: {
+    'prettier/prettier': ['error', { semi: false }],
+    'no-console': 'error',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  ignorePatterns: ['.eslintrc.cjs'],
+  settings: {
+    node: {
+      version: '>=16.0.0',
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.spec.ts', '**/*.test.js', '**/*.spec.js'],
+      rules: {
+        '@typescript-eslint/no-unsafe-call': 'off',
+      },
+    },
+  ],
+}
---
+++ jest.config.js
@@ -0,0 +1,28 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/src/**/?(*.)+(spec|test).[tj]s?(x)'],
+  verbose: true,
+  transform: {
+    '^.+\\.[jt]s?$': [
+      'ts-jest',
+      {
+        tsConfig: 'tsconfig.spec.json',
+        isolatedModules: false,
+      },
+    ],
+  },
+  moduleFileExtensions: [`js`, `jsx`, `ts`, `tsx`, `json`],
+  watchPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/setupTests.js'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{js,ts,tsx,jsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+}
