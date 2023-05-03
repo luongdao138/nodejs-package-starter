8426927a5b3ea8ef8ae3fa00f07e0502d977a9bb
luongdao <luong.dao@pionero.io>
2023-05-03 19:26:19 +0700

    add test validation

--- src/api/routes/admin/user/get-users.ts
+++ src/api/routes/admin/user/get-users.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from 'express'
+import { z } from 'zod'
+
+const userSchema = z.object({
+  name: z.string().min(10),
+  age: z.number().min(10),
+})
 
 export default async function (req: Request, res: Response) {
-  res.json({ msg: 'Tada! Welcome to users route!' })
+  const validated = userSchema.parse({
+    name: 'dao van luong',
+    age: 21,
+  })
+  res.json([validated])
 }
